3a6541b07d1f40bd53ed3e501f14e1b53ccdc05f
adarsh darokar <adarshdarokar55@gmail.com>
2025-07-15 22:47:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+function solve(ans,curr, i ,arr){
+    if (i === arr.length){
+        ans.push ([...curr]);
+    }
+ 
+    curr.push(arr [i]);
+    solve(ans,curr,i+1,arr);
+    curr.pop();
+    while (i+1 <arr.length && arr[i] === arr[i +1]){
+        i++;
+    }
+        curr.pop();
+    solve =(ans,curr,i+1,arr);
+}
+let arr= [1,2,2];
+let ans = [];
+solve (ans,[],0,arr);
+console.log(ans);
